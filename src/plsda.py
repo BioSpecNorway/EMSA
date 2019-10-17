e309cc459f813425976ea37d642f302ebc802b9c
Vlad Blazhko <mr_plum@mail.ru>
2019-10-17 17:10:42 +0200

    Add documentation to plsda

--- src/plsda.py
+++ src/plsda.py
@@ -5,16 +5,29 @@ import numpy as np
 
 
 class PLSDADummy(BaseEstimator):
+    """
+    Wrapper of PLSRegression for classification.
+
+    PLSRegression predicts one hot encoded vectors,
+    then plsda outputs class with maximal score.
+    """
     def __init__(self, n_components=2):
         self.pls = PLSRegression(n_components)
         self.classes = None
 
     def __one_hot_encode(self, Y):
+        # encode labels to numbers
         Y = np.array([np.where(self.classes == y)[0][0] for y in Y])
+
         enc = OneHotEncoder(n_values=len(self.classes))
         return enc.fit_transform(Y.reshape(-1, 1)).toarray()
 
     def fit(self, X, Y):
+        """
+
+        :param X:
+        :param Y: list of labels
+        """
         self.classes = np.array(sorted(np.unique(Y)))
         Y = self.__one_hot_encode(Y)
         self.pls.fit(X, Y)
